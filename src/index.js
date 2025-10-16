f03d7767931e92f00e153b63fd0ea874dd3723f2
Raghad Islam <raghad.noser.2003@gmail.com>
2025-10-16 19:45:43 +0300

    add Header, Footer and Menu Components

--- src/index.js
+++ src/index.js
@@ -49,10 +49,9 @@ const pizzaData = [
 function App() {
   return (
     <div>
-      <h1>Hello, world!</h1>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Header />
+      <Menu />
+      <Footer />
     </div>
   );
 }
@@ -62,8 +61,30 @@ function Pizza() {
     <dev>
       <img src="pizzas/spinaci.jpg" alt="pizza spinaci" />
       <h1>Pizza Spinaci</h1>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>;
+      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
     </dev>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our Menu</h2>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>{new Date().toLocaleTimeString()}We're currently open!</footer>
   );
 }
 
